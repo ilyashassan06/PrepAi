36a4f1c847dc48354ac96122e352acc96e9ab980
Ilyas hassan <iamilyashassan06@gmail.com>
2026-07-09 00:05:48 +0530

    layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import InterviewSetup from "./pages/InterviewSetup"
-import Interview from "./pages/Interview"
-import Result from "./pages/Result"
-import History from "./pages/History"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer"
-
-
-
+import { Outlet, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import InterviewSetup from "./pages/InterviewSetup";
+import Interview from "./pages/Interview";
+import Result from "./pages/Result";
+import History from "./pages/History";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Layout from "./Layout";
 
 function App() {
- 
-
   return (
     <>
       <Routes>
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Signup" element={<Signup/>} />
-    </Routes>
-
-
-      <Navbar/>
-     <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/Dashboard" element={<Dashboard/>} />
-        <Route path="/InterviewSetup" element={<InterviewSetup/>} />
-        <Route path="/Interview" element={<Interview/>} />
-        <Route path="/Result" element={<Result/>} />
-        <Route path="/History" element={<History/>} />
-    </Routes>
-    <Footer/>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/InterviewSetup" element={<InterviewSetup />} />
+          <Route path="/Interview" element={<Interview />} />
+          <Route path="/Result" element={<Result />} />
+          <Route path="/History" element={<History />} />
+        </Route>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
